bcacee71a69328eb7cc71640eea165eec0dbf725
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-20 12:07:34 +0200

    2-headers: get random joke

--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -8,15 +8,19 @@ const Headers = () => {
   const [joke, setJoke] = useState('random dad joke');
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    axios(url, {
+      headers: {
+        Accept: 'application/json',
+      },
+    }).then((res) => setJoke(res.data.joke));
   };
 
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
+    <section className="section text-center">
+      <button className="btn" onClick={fetchDadJoke}>
         random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+      <p className="dad-joke">{joke}</p>
     </section>
   );
 };
