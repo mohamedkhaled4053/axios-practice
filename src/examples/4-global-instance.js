c628a5289f08d202c0c0fd4660539d488b30e708
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-08-20 18:19:02 +0200

    4-global-instance: add global default headers

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,23 @@
 import { useEffect } from 'react';
+import axios from 'axios';
+import '../axios/global'
 
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const resp1 = await axios(productsUrl);
+      const resp2 = await axios(randomUserUrl);
+      console.log(resp1,resp2);
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className='text-center' onClick={fetchData}>global instance</h2>;
 };
 export default GlobalInstance;
